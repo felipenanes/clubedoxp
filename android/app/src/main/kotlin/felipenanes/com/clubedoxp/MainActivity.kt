ad9cc3a4c1c45eb869260587caec8c4d6cb5411b
felipe.gomes.nanes <felipenanes@gmail.com>
2020-03-04 15:38:31 -0300

    Initial commit

---
+++ android/app/src/main/kotlin/felipenanes/com/clubedoxp/MainActivity.kt
@@ -0,0 +1,13 @@
+package felipenanes.com.clubedoxp
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
